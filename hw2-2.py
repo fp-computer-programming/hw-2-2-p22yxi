568d58b8ef26af3fa826832ac88d7a8d7f7bb501
p22yxi <p22yxi@fairfieldprep.org>
2021-09-23 11:11:26 -0400

    hw2-2

---
+++ hw2-2.py
@@ -0,0 +1,7 @@
+# Author : Yongdong Xi (Sep 22 2021)
+
+free = float(input("How many free throw"))
+inside = float(input("How many basket inside the three point line"))
+beyond = float(input("How many basket beyond the three point line"))
+a = free * 1 + inside * 2 + beyond * 3
+print(a) 
